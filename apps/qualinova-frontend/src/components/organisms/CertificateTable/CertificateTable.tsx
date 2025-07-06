17e9cbae3a0aff796fa8678de3ab76a1ce23bd65
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2025-07-06 22:22:36 +0100

    fix: certificates page layout improvements

--- apps/qualinova-frontend/src/components/organisms/CertificateTable/CertificateTable.tsx
+++ apps/qualinova-frontend/src/components/organisms/CertificateTable/CertificateTable.tsx
@@ -1,12 +1,18 @@
-import CertificateRow from "@/components/molecules/CertificateRow/CertificateRow";
+import CertificateRow from '@/components/molecules/CertificateRow/CertificateRow';
+import { ArrowUpDown } from 'lucide-react';
 
 const CertificateTable = ({ certificates }: { certificates: any[] }) => (
-  <div className="border-2 border-[#20293c] flex justify-center rounded-lg p-4 w-full">
+  <div className="border-2 border-dark-blue-border flex justify-center rounded-lg p-4 w-full">
     <table className="table-auto w-3/4 border-collapse">
-      <thead>
-        <tr className="space-x-3 border-[#20293c] *:py-5 text-left">
+      <thead className="text-dark-blue-text text-sm">
+        <tr className="space-x-3 border-dark-blue-border *:py-5 text-left">
           <th>ID</th>
-          <th className="w-1/4">Name</th>
+          <th className="w-1/4 flex items-center gap-2">
+            Name
+            <span>
+              <ArrowUpDown className="h-4 w-4 text-dark-blue-text" />
+            </span>
+          </th>
           <th>Recipient</th>
           <th>Issue Date</th>
           <th>Expiry Date</th>
--- apps/qualinova-frontend/src/components/templates/certificate/CertificateDashboard.tsx
+++ apps/qualinova-frontend/src/components/templates/certificate/CertificateDashboard.tsx
@@ -1,15 +1,16 @@
-"use client";
+'use client';
 
-import Button from "@/components/atoms/Button/Button";
-import Select from "@/components/atoms/Select/Select";
-import CertificateTable from "@/components/organisms/CertificateTable/CertificateTable";
-import Image from "next/image";
-import React, { useState } from "react";
+import Button from '@/components/atoms/Button/Button';
+import Select from '@/components/atoms/Select/Select';
+import CertificateTable from '@/components/organisms/CertificateTable/CertificateTable';
+import { Funnel, RefreshCw, Search } from 'lucide-react';
+import Image from 'next/image';
+import React, { useState } from 'react';
 
 enum Status {
-  Active = "Active",
-  Pending = "Pending",
-  Expired = "Expired",
+  Active = 'Verified',
+  Pending = 'Pending',
+  Expired = 'Expired',
 }
 
 interface Certificate {
@@ -23,96 +24,96 @@ interface Certificate {
 }
 
 type SortOption =
-  | "All Certificates"
-  | "Sort by Issuance Date"
-  | "Sort by Expiry Date"
-  | "Sort by ID";
+  | 'All Certificates'
+  | 'Sort by Issuance Date'
+  | 'Sort by Expiry Date'
+  | 'Sort by ID';
 
 const initialCertificates: Certificate[] = [
   {
-    id: "CERT-2023-001",
-    name: "ISO 9001 Compliance",
-    recipient: "Acme Corporation",
-    issue_date: "14/12/2023",
-    expiry_date: "9/12/2024",
+    id: 'CERT-2023-001',
+    name: 'ISO 9001 Compliance',
+    recipient: 'Acme Corporation',
+    issue_date: '14/12/2023',
+    expiry_date: '9/12/2024',
     status: Status.Active,
-    actions: "...",
+    actions: '...',
   },
   {
-    id: "CERT-2023-002",
-    name: "ISO 9001 Compliance",
-    recipient: "Acme Corporation",
-    issue_date: "9/12/2023",
-    expiry_date: "9/12/2024",
+    id: 'CERT-2023-002',
+    name: 'ISO 9001 Compliance',
+    recipient: 'Acme Corporation',
+    issue_date: '9/12/2023',
+    expiry_date: '9/12/2024',
     status: Status.Active,
-    actions: "...",
+    actions: '...',
   },
   {
-    id: "CERT-2023-003",
-    name: "ISO 9001 Compliance",
-    recipient: "Acme Corporation",
-    issue_date: "4/12/2023",
-    expiry_date: "4/12/2024",
+    id: 'CERT-2023-003',
+    name: 'ISO 9001 Compliance',
+    recipient: 'Acme Corporation',
+    issue_date: '4/12/2023',
+    expiry_date: '4/12/2024',
     status: Status.Pending,
-    actions: "...",
+    actions: '...',
   },
   {
-    id: "CERT-2023-004",
-    name: "ISO 9001 Compliance",
-    recipient: "Acme Corporation",
-    issue_date: "19/11/2023",
-    expiry_date: "19/11/2024",
+    id: 'CERT-2023-004',
+    name: 'ISO 9001 Compliance',
+    recipient: 'Acme Corporation',
+    issue_date: '19/11/2023',
+    expiry_date: '19/11/2024',
     status: Status.Expired,
-    actions: "...",
+    actions: '...',
   },
   {
-    id: "CERT-2023-005",
-    name: "ISO 9001 Compliance",
-    recipient: "Acme Corporation",
-    issue_date: "14/11/2023",
-    expiry_date: "14/11/2024",
+    id: 'CERT-2023-005',
+    name: 'ISO 9001 Compliance',
+    recipient: 'Acme Corporation',
+    issue_date: '14/11/2023',
+    expiry_date: '14/11/2024',
     status: Status.Active,
-    actions: "...",
+    actions: '...',
   },
   {
-    id: "CERT-2023-006",
-    name: "ISO 9001 Compliance",
-    recipient: "Acme Corporation",
-    issue_date: "9/11/2023",
-    expiry_date: "9/11/2024",
+    id: 'CERT-2023-006',
+    name: 'ISO 9001 Compliance',
+    recipient: 'Acme Corporation',
+    issue_date: '9/11/2023',
+    expiry_date: '9/11/2024',
     status: Status.Active,
-    actions: "...",
+    actions: '...',
   },
   {
-    id: "CERT-2023-007",
-    name: "ISO 9001 Compliance",
-    recipient: "Acme Corporation",
-    issue_date: "4/11/2023",
-    expiry_date: "4/11/2024",
+    id: 'CERT-2023-007',
+    name: 'ISO 9001 Compliance',
+    recipient: 'Acme Corporation',
+    issue_date: '4/11/2023',
+    expiry_date: '4/11/2024',
     status: Status.Pending,
-    actions: "...",
+    actions: '...',
   },
   {
-    id: "CERT-2023-008",
-    name: "ISO 9001 Compliance",
-    recipient: "Acme Corporation",
-    issue_date: "10/24/2023",
-    expiry_date: "10/24/2024",
+    id: 'CERT-2023-008',
+    name: 'ISO 9001 Compliance',
+    recipient: 'Acme Corporation',
+    issue_date: '10/24/2023',
+    expiry_date: '10/24/2024',
     status: Status.Active,
-    actions: "...",
+    actions: '...',
   },
 ];
 
 const CertificateDashboard = () => {
   const [certificates, setCertificates] = useState(initialCertificates);
   const [total, setTotal] = useState(certificates.length);
-  const [max, setMax] = useState(9);
+  const [max, setMax] = useState(certificates.length);
 
   const sort: Record<SortOption, (msg: string) => void> = {
-    "All Certificates": (msg: string) => console.log(msg),
-    "Sort by Issuance Date": (msg: string) => console.log(msg),
-    "Sort by Expiry Date": (msg: string) => console.log(msg),
-    "Sort by ID": (msg: string) => console.log(msg),
+    'All Certificates': (msg: string) => console.log(msg),
+    'Sort by Issuance Date': (msg: string) => console.log(msg),
+    'Sort by Expiry Date': (msg: string) => console.log(msg),
+    'Sort by ID': (msg: string) => console.log(msg),
   };
 
   return (
@@ -120,14 +121,14 @@ const CertificateDashboard = () => {
       {/* Header */}
       <div className="flex justify-between items-center w-full">
         <div className="text-xl font-bold">Certificates</div>
-        <Button>
+        <Button variant="secondary">
           <Image src="/newCert.svg" alt="+" width={20} height={20} />
           <span>New Certificate</span>
         </Button>
       </div>
 
       {/* Management Panel */}
-      <div className="border-2 border-[#20293c] p-4 space-y-5 rounded-lg">
+      <div className="border border-dark-blue-border p-4 space-y-5 rounded-lg">
         <div>
           <div>Certificate Management</div>
           <div className="text-[#717c91] text-xs">
@@ -136,53 +137,36 @@ const CertificateDashboard = () => {
         </div>
 
         {/* Filter and Search */}
-        <div className="flex space-x-3">
-          <div className="p-2 w-full flex border-2 border-[#20293c] space-x-3 rounded-lg">
-            <Image
-              src="/search.svg"
-              alt="+"
-              width={20}
-              height={20}
-              className=""
-            />
+        <div className="flex">
+          <div className="h-10 px-2 w-full flex border border-dark-blue-border gap-2 rounded-lg items-center">
+            <Search className="text-dark-blue-text text-xs h-5 w-5" />
             <input
               type="search"
-              placeholder="Search"
-              className="bg-inherit lg:w-full focus:outline-none"
+              placeholder="Search certificates..."
+              className="bg-inherit lg:w-full focus:outline-none placeholder:text-sm"
             />
           </div>
 
-          <div className="flex *:border-2 *:border-[#20293c] *:rounded-lg space-x-3">
-            <div className="flex space-x-3 items-center px-2">
-              <Image
-                src="/filter.svg"
-                alt="+"
-                width={20}
-                height={20}
-                className=""
-              />
+          <div className="flex h-10 items-center px-4 gap-2">
+            <div className="flex border h-10 border-dark-blue-border rounded-lg items-center px-3 gap-2">
+              <Funnel className="text-xs h-5 w-5" />
               <Select
                 onChange={(e) => {
                   const key = e.target.value as SortOption;
-                  sort[key](key + " from sort function");
+                  sort[key](key + ' from sort function');
                 }}
-                className="w-40 appearance-none  border-none  focus-visible:ring-0 focus:outline-none bg-inherit"
+                className="w-40 appearance-none border-none focus-visible:ring-0 focus:outline-none bg-inherit hover:cursor-pointer"
               >
                 {Object.keys(sort).map((key) => (
-                  <option key={key} value={key} className="text-black">
+                  <option key={key} value={key} className="text-black text-xs">
                     {key}
                   </option>
                 ))}
               </Select>
             </div>
-            <div className="flex items-center w-14 p-3">
-              <Image
-                src="/reset.svg"
-                alt="+"
-                width={45}
-                height={45}
-                className="w-full"
-              />
+
+            <div className="flex h-10 border border-dark-blue-border rounded-lg items-center px-3 hover:cursor-pointer">
+              <RefreshCw className="text-xs h-5 w-5" />
             </div>
           </div>
         </div>
@@ -191,13 +175,17 @@ const CertificateDashboard = () => {
         <CertificateTable certificates={certificates} />
 
         {/* Footer */}
-        <div className="flex justify-between">
-          <div>
-            showing {total} out of {max} certificates
+        <div className="flex justify-between items-center">
+          <div className="text-dark-blue-text text-sm">
+            Showing {total} out of {max} certificates
           </div>
-          <div className="flex *:border-2 space-x-3 *:rounded-lg *:p-2 *:border-[#20293c]">
-            <button type="button">Previous</button>
-            <button type="button">Next</button>
+          <div className="flex gap-4">
+            <Button type="button" variant="outline">
+              Previous
+            </Button>
+            <Button type="button" variant="outline">
+              Next
+            </Button>
           </div>
         </div>
       </div>
