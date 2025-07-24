73a2d031c893b8d457ba24403e71e71972b3e1ad
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2025-07-25 00:14:07 +0100

    clean up

--- apps/qualinova-frontend/src/components/organisms/MainPage/HowItWorks/howItWorks.test.tsx
+++ apps/qualinova-frontend/src/components/organisms/MainPage/HowItWorks/howItWorks.test.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { render, screen, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import HowItWorks from "./howItWorks";
-import { get } from "http";
 
 // Mock Next.js router if needed
 jest.mock("next/router", () => ({
