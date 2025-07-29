68943de018cefb9fb5a1c1ea485773e1dc057ded
Luis Rojas <luis.4.2000@hotmail.com>
2025-07-28 19:51:41 -0600

    feat: update import

--- apps/qualinova-frontend/src/app/dashboard/page.tsx
+++ apps/qualinova-frontend/src/app/dashboard/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import Dashboard from "@/components/organisms/Dashboard/Dashboard";
+import Dashboard from "@/components/organisms/Dashboard/dashboard";
 export default function DashboardPage() {
   return <Dashboard />;
 }
